8bdf28528984ec1d7d47284b7b4de6602d1e56f8
Wolf McNally <wolf@wolfmcnally.com>
2023-12-01 19:54:11 -0800

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.9
 
 import PackageDescription
 
@@ -15,18 +14,10 @@ let package = Package(
             targets: ["Esplora"]),
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/WolfMcNally/WolfBase",
-            from: "5.0.0"
-        ),
-        .package(
-            url: "https://github.com/WolfMcNally/WolfAPI",
-            from: "2.0.0"
-        ),
+        .package(url: "https://github.com/WolfMcNally/WolfBase", from: "6.0.0"),
+        .package(url: "https://github.com/WolfMcNally/WolfAPI", from: "3.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Esplora",
             dependencies: ["WolfBase", "WolfAPI"]),
